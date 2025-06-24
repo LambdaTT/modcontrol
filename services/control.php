81d417ecc5f0e165b5d154574438250d57b1fe40
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2025-06-24 01:18:49 -0300

    added service

--- services/control.php
+++ services/control.php
@@ -10,21 +10,37 @@ class ModControlService extends Service
 
   public function list($params = [])
   {
-    return $this->getDao('APM_MODULE')
+    return $this->getDao('MDC_MODULE')
       ->bindParams($params)
       ->find(
         "SELECT 
           ds_title, 
           dt_created, 
-          id_apm_module 
-        FROM `APM_MODULE`"
+          id_mdc_module 
+        FROM `MDC_MODULE`"
       );
   }
 
   public function get($params = [])
   {
-    return $this->getDao('APM_MODULE')
+    return $this->getDao('MDC_MODULE')
       ->bindParams($params)
       ->first();
+  }
+
+  public function getModuleEntities($modParams)
+  {
+    return $this->getDao('MDC_MODULE_ENTITY')
+      ->bindParams($modParams, 'modFilters')
+      ->find(
+        "SELECT 
+            ent.*,
+            mod.ds_title AS modTitle 
+          FROM `MDC_MODULE_ENTITY` ent
+          JOIN (
+            SELECT * FROM `MDC_MODULE` 
+            #modFilters#
+          ) mod ON ent.id_mdc_module = mod.id_mdc_module"
+      );
   }
 }
